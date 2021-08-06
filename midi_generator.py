edde00c6c5b7b6ceae7446d45a584dc8fa0fe738
Kostoski Stefan <kostoski.stefan90@gmail.com>
2021-08-06 09:59:46 +0200

    Lower note pitch

--- midi_generator.py
+++ midi_generator.py
@@ -1,28 +1,29 @@
 from midiutil import MIDIFile
-from time import sleep
 
 note_start_time = 0
 track = 0
 channel = 0
+duration = 0.5
 velocity = 127
+tempo = 170
 
 midi_notes = {
-    "C": 60,
-    "C#": 61,
-    "D": 62,
-    "D#": 63,
-    "E": 64,
-    "F": 65,
-    "F#": 66,
-    "G": 67,
-    "G#": 68,
-    "A": 69,
-    "A#": 70,
-    "B": 71,
+    "C":    48,
+    "C#":   49,
+    "D":    50,
+    "D#":   51,
+    "E":    52,
+    "F":    53,
+    "F#":   54,
+    "G":    55,
+    "G#":   56,
+    "A":    57,
+    "A#":   58,
+    "B":    59,
 }
 
 midi_file = MIDIFile(1)
-midi_file.addTempo(0, 0, 120)
+midi_file.addTempo(track, note_start_time, tempo)
 
 
 def play_chord(chord: str):
@@ -40,15 +41,15 @@ def play_minor_chord(root_note: int):
     global note_start_time
 
     for i in range(2):
-        midi_file.addNote(track, channel, root_note, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note, note_start_time, duration,  velocity)
         note_start_time += 1
-        midi_file.addNote(track, channel, root_note + 3, note_start_time, 1, velocity)
-        midi_file.addNote(track, channel, root_note + 7, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note + 3, note_start_time, duration,  velocity)
+        midi_file.addNote(track, channel, root_note + 7, note_start_time, duration,  velocity)
         note_start_time += 1
-        midi_file.addNote(track, channel, root_note - 5, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note - 5, note_start_time, duration,  velocity)
         note_start_time += 1
-        midi_file.addNote(track, channel, root_note + 3, note_start_time, 1, velocity)
-        midi_file.addNote(track, channel, root_note + 7, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note + 3, note_start_time, duration,  velocity)
+        midi_file.addNote(track, channel, root_note + 7, note_start_time, duration,  velocity)
         note_start_time += 1
 
 
@@ -56,15 +57,15 @@ def play_major_chord(root_note: int):
     global note_start_time
 
     for i in range(2):
-        midi_file.addNote(track, channel, root_note, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note, note_start_time, duration,  velocity)
         note_start_time += 1
-        midi_file.addNote(track, channel, root_note + 4, note_start_time, 1, velocity)
-        midi_file.addNote(track, channel, root_note + 7, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note + 4, note_start_time, duration,  velocity)
+        midi_file.addNote(track, channel, root_note + 7, note_start_time, duration,  velocity)
         note_start_time += 1
-        midi_file.addNote(track, channel, root_note - 5, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note - 5, note_start_time, duration,  velocity)
         note_start_time += 1
-        midi_file.addNote(track, channel, root_note + 4, note_start_time, 1, velocity)
-        midi_file.addNote(track, channel, root_note + 7, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note + 4, note_start_time, duration,  velocity)
+        midi_file.addNote(track, channel, root_note + 7, note_start_time, duration,  velocity)
         note_start_time += 1
 
 
@@ -72,15 +73,15 @@ def play_seventh_chord(root_note: int):
     global note_start_time
 
     for i in range(2):
-        midi_file.addNote(track, channel, root_note, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note, note_start_time, duration,  velocity)
         note_start_time += 1
-        midi_file.addNote(track, channel, root_note + 4, note_start_time, 1, velocity)
-        midi_file.addNote(track, channel, root_note + 7, note_start_time, 1, velocity)
-        midi_file.addNote(track, channel, root_note + 10, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note + 4, note_start_time, duration,  velocity)
+        midi_file.addNote(track, channel, root_note + 7, note_start_time, duration,  velocity)
+        midi_file.addNote(track, channel, root_note + 10, note_start_time, duration,  velocity)
         note_start_time += 1
-        midi_file.addNote(track, channel, root_note - 5, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note - 5, note_start_time, duration,  velocity)
         note_start_time += 1
-        midi_file.addNote(track, channel, root_note + 4, note_start_time, 1, velocity)
-        midi_file.addNote(track, channel, root_note + 7, note_start_time, 1, velocity)
-        midi_file.addNote(track, channel, root_note + 10, note_start_time, 1, velocity)
+        midi_file.addNote(track, channel, root_note + 4, note_start_time, duration,  velocity)
+        midi_file.addNote(track, channel, root_note + 7, note_start_time, duration,  velocity)
+        midi_file.addNote(track, channel, root_note + 10, note_start_time, duration,  velocity)
         note_start_time += 1
